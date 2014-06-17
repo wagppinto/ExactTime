5bfe08edc9b8b11ba7f55f81231199834ad2beed
Joshua Howland <development@jkhowland.com>
2014-06-17 00:31:17 -0600

    Adds AFNetworking as submodule and makes request

--- ExactTime/ETViewController.m
+++ ExactTime/ETViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ETViewController.h"
+#import "AFNetworking.h"
 
 @interface ETViewController ()
 
@@ -18,6 +19,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.timeapi.org/utc/now"]];
+
+    AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    operation.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"%@", [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"%@",error.userInfo[NSLocalizedDescriptionKey]);
+    }];
+    
+    [operation start];
 }
 
 - (void)didReceiveMemoryWarning
